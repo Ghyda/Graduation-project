34ba18653edb8907f512169051cb9de973e2cced
Ghyda <ghyda.alamri@gmail.com>
2016-10-24 18:48:06 +0300

    Add files via upload

---
+++ mysite/questions/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'questions'
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/answers/$', views.AnswersView.as_view(), name='answers'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+]
