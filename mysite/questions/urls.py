83b71be12bb41f0bb3f93a17a64cf0f21d7afa19
Ghyda <ghyda.alamri@gmail.com>
2016-10-26 19:30:29 +0300

    Update urls.py

--- mysite/questions/urls.py
+++ mysite/questions/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/answers/$', views.AnswersView.as_view(), name='answers'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
 ]
